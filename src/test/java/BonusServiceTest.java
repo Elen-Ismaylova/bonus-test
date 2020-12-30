a214fbe8bfc8ffae7f590695ab8edbf1e39e1e01
Elen Ismaylova <wonderelen@gmail.com>
2020-12-30 11:29:45 +0300

    new file

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -12,7 +12,7 @@ class BonusServiceTest {
         BonusService service = new BonusService();
 
         long amount = 1000_60;
-        boolean registered = false;
+        boolean registered = true;
         long expected = 30;
         long actual = service.calculate(amount, registered);
 
